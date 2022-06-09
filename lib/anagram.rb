cdde77bab24d26ce92168a694d8b60f6c060a1b9
jesse zenon <jesse.zenon@gmail.com>
2022-06-08 20:38:33 -0500

    commit tree f37547eebf4849ac1a9ce27b083c08cc880e7924

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :words 
+    def initialize(words)
+        words.each do |word|
+              letters = word.chars
+                ogWords = word.chars 
+            if letters.sort == ogWords.sort 
+                puts true
+            else 
+                puts false
+            end 
+    end
+end
+
+a1 = Anagram.new
